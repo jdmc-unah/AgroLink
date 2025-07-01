6997647bb2f09263ff222d979cc763b284ce86b6
mar <kisha.mejia@unah.hn>
2025-07-01 11:03:25 -0600

    tablasTransaccionSchemaPruebas.sql

--- sql_querys/trablas_transaccion.sql
+++ sql_querys/trablas_transaccion.sql
@@ -6,205 +6,217 @@ select * from AgroLinkDB.Pruebas.Socio
 
 --creacion de tablas de transacciones
 
+/*
+bases padre
+drop table Pruebas.Venta
+drop table Pruebas.Compra
+drop table Pruebas.EntradaProducto
+drop table Pruebas.Factura
+drop table Pruebas.Recibo
+drop table Pruebas.SalidaProducto
+
+bases hija
+drop table Pruebas.CompraDetalle
+drop table Pruebas.VentaDetalle
+drop table Pruebas.EntradaProductoDetalle
+drop table Pruebas.FacturaDetalle
+drop table Pruebas.ReciboDetalle
+drop table Pruebas.SalidaProductoDetalle
+*/
 
 --VENTAS
 
-CREATE TABLE Pruebas.Ventas(
-VentasID int primary key not null,
-Fecha Datetime not null,
-SocioID int not null,
-ListaPreciosID int not null,
-TipoPago varchar(50) not null,
-Estado varchar(50) not null,
+CREATE TABLE Pruebas.Venta --ya creada
+(
+	VentaID int primary key not null,
+	Fecha Datetime not null,
+	SocioID int not null,
+	ListaPreciosID int not null,
+	TipoPago varchar(50) not null,
+	Estado varchar(50) not null,
 
-constraint chkTipoPago check( TipoPago in('Contado','Credito') ),
-constraint chkEstadoVenta check( Estado in('Abierto','Cerrado','Cancelado') ),
-Constraint fkVentasSocios FOREIGN KEY (SocioID) REFERENCES Pruebas.Socio(SocioID),
-constraint fkVebtasListaPrecios FOREIGN key (ListaPreciosID) REFERENCES Pruebas.ListaPrecios(ListaPreciosID)
+	constraint chkTipoPago check( TipoPago in('Contado','Credito') ),
+	constraint chkEstadoVenta check( Estado in('Abierto','Cerrado','Cancelado') ),
+	
+	constraint fkVentaSocios FOREIGN KEY (SocioID) REFERENCES Pruebas.Socio(SocioID),
+	constraint fkVentaListaPrecios FOREIGN key (ListaPreciosID) REFERENCES Pruebas.ListaPrecios(ListaPreciosID)
 )
 go
 
 --VentasDetalle 
 
-Create Table Pruebas.VentasDetalle(
-VentasID int primary key  not null,
-ProductoID int not null,
-ImpuestoID int not null,
-BodegaID int not null,
-Cantidad int not null,
-Precio decimal(10,2) not null default 0 check (Precio >= 0),
-Total decimal(10,2) not null default 0 check (Total >= 0),
-constraint fkVentasDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
-constraint fkVentasDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
-constraint fkVentasDetalleBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID)
-
-
+Create Table Pruebas.VentaDetalle --ya creada
+(
+	VentaID int not null,
+	ProductoID int not null,
+	ImpuestoID int not null,
+	BodegaID int not null,
+	Cantidad int not null,
+	Precio decimal(10,2) not null default 0 check (Precio >= 0),
+	Total decimal(10,2) not null default 0 check (Total >= 0),
+	
+	constraint pkVentaDetalle primary key (VentaID, ProductoID), 
+	constraint fkVentaDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
+	constraint fkVentaDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
+	constraint fkVentaDetalleBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID)
 )
 go
 
 --Compras
 --Compra es el registro de la compra al credito, recibo seria cuando ya se pague
-Create Table Pruebas.Compras(
-CompraID int primary key not null,
-ListaPreciosID int not null,
-Fecha Datetime not null,
-SocioID int not null,
-TipoPago varchar(50) not null,
-Estado varchar(50) not null,
+Create Table Pruebas.Compra --ya creada
+(
+	CompraID int primary key not null,
+	ListaPreciosID int not null,
+	Fecha Datetime not null,
+	SocioID int not null,
+	TipoPago varchar(50) not null,
+	Estado varchar(50) not null,
 
+	constraint chkTipoPagoCompra check( TipoPago in('Contado','Credito') ),
+	constraint chkEstadoVentaCompra check( Estado in('Abierto','Cerrado','Cancelado') ),
 
-constraint fkComprasListaPrecios foreign key (ListaPreciosID) references Pruebas.ListaPrecios(ListaPreciosID),
-constraint fkComprasSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
-
-
-constraint chkTipoPagoCompras check( TipoPago in('Contado','Credito') ),
-constraint chkEstadoVentaCompras check( Estado in('Abierto','Cerrado','Cancelado') )
-
+	constraint fkCompraListaPrecios foreign key (ListaPreciosID) references Pruebas.ListaPrecios(ListaPreciosID),
+	constraint fkCompraSocio foreign key (SocioID) references Pruebas.Socio(SocioID)
 )
 go 
 
 --CompraDetalle
 
-Create table Pruebas.CompraDetalle(
-CompraID int primary key not null,
-ProductoID int not null,
-ImpuestoID int not null,
-BodegaID int not null,
-Cantidad int not null,
-Precio decimal(10,2) not null default 0 check (Precio >= 0),
-Total decimal(10,2) not null default 0 check (Total >= 0),
+Create table Pruebas.CompraDetalle --ya creada
+(
+	CompraID int not null,
+	ProductoID int not null,
+	ImpuestoID int not null,
+	BodegaID int not null,
+	Cantidad int not null,
+	Precio decimal(10,2) not null default 0 check (Precio >= 0),
+	Total decimal(10,2) not null default 0 check (Total >= 0),
 
-constraint fkComprasDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
-constraint fkComprasDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
-constraint fkComprasDetalleBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
-
-
-
+	constraint pkCompraDetalle primary key (CompraID, ProductoID),
+	constraint fkCompraDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
+	constraint fkCompraDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
+	constraint fkCompraDetalleBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
 )
 go
 
 --EntradProducto
 
-Create Table Pruebas.EntradaProducto(
-EntradaID int primary key not null,
-SocioID int not null,
-BodegaID int not null,
-Fecha Datetime not null,
-LoteID int,
-Credito float not null,
+Create Table Pruebas.EntradaProducto --ya creada
+(
+	EntradaID int primary key not null,
+	SocioID int not null,
+	BodegaID int not null,
+	Fecha Datetime not null,
+	LoteID int,
+	Credito decimal(10,2) not null,
 
-constraint fkEntradaProductoSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
-constraint fkEntradaProductoBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
-constraint fkEntradaProductoLote foreign key (LoteID) references Pruebas.Lote(LoteID),
+	constraint fkEntradaProductoSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
+	constraint fkEntradaProductoBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
+	constraint fkEntradaProductoLote foreign key (LoteID) references Pruebas.Lote(LoteID),
 
 )
 go
 
 --Entrada Producto Detalle
 
-Create Table Pruebas.EntradaProductoDetalle(
-EntradaID int primary key not null,
-ProductoID int not null,
-Cantidad int not null,
+Create Table Pruebas.EntradaProductoDetalle --ya creada
+(
+	EntradaID int not null,
+	ProductoID int not null,
+	Cantidad int not null,
 
-constraint fkEntradaProductoDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
-
+	constraint pkEntradaProductoDetalle primary key (EntradaID, ProductoID),
+	constraint fkEntradaProductoDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID)
 )
 go
 
 --Factura
 --por aqui me quede en creacion
-Create Table Pruebas.Factura(
-FacturaID int primary key not null,
-SocioID int not null,
-ListaPreciosID int not null,
-EmpresaID int not null,
-Fecha Datetime not null,
-MetodoPago varchar(50) not null,
-CAI varchar(50) not null,
-constraint chkMetodoPago check( MetodoPago in('Efectivo','Tarjeta','Cheque') ),
+Create Table Pruebas.Factura --ya creada
+(
+	FacturaID int primary key not null,
+	SocioID int not null,
+	ListaPreciosID int not null,
+	EmpresaID int not null,
+	Fecha Datetime not null,
+	MetodoPago varchar(50) not null,
+	CAI varchar(50) not null,
+	constraint chkMetodoPago check( MetodoPago in('Efectivo','Tarjeta','Cheque') ),
 
-constraint fkFacturaSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
-constraint fkFacturaListaPrecios foreign key (ListaPreciosID) references Pruebas.ListaPrecios(ListaPreciosID),
-constraint fkFacturaEmpresa foreign key (EmpresaID) references Pruebas.Empresa(EmpresaID),
-
-
-
+	constraint fkFacturaSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
+	constraint fkFacturaListaPrecios foreign key (ListaPreciosID) references Pruebas.ListaPrecios(ListaPreciosID),
+	constraint fkFacturaEmpresa foreign key (EmpresaID) references Pruebas.Empresa(EmpresaID),
 )
 
 --FacturaDetalle
 
-Create Table Pruebas.FacturaDetalle(
-FacturaID int primary key not null,
-ProductoID int not null,
-ImpuestoID int not null,
-BodegaID int not null,
-Cantidad int not null,
-Precio float not null,
-Total float not null
+Create Table Pruebas.FacturaDetalle --ya creada
+(
+	FacturaID int not null,
+	ProductoID int not null,
+	ImpuestoID int not null,
+	BodegaID int not null,
+	Cantidad int not null,
+	Precio decimal(10, 2) not null,
+	Total decimal(10, 2) not null
 
-constraint fkFacturaDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
-constraint fkFacturaDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
-constraint fkFacturaDetalleBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
+	constraint pkFacturaDetalle primary key (FacturaID, ProductoID),
 
-
-
-
+	constraint fkFacturaDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
+	constraint fkFacturaDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
+	constraint fkFacturaDetalleBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID)
 )
 
-Create Table Pruebas.Recibo(
-ReciboID int primary key not null,
-ListaPreciosID int not null,
-SocioID int not null,
-MetodoPago varchar(50) not null,
-Fecha Datetime not null,
-constraint chkMetodoPagoRecibo check( MetodoPago in('Efectivo','Tarjeta','Cheque') ),
 
-constraint fkReciboListaPreciosID foreign key (ListaPreciosID) references Pruebas.ListaPrecios(ListaPreciosID),
-constraint fkReciboSocio foreign key (SocioID) references Pruebas.Socio(SocioID)
+Create Table Pruebas.Recibo --ya creada
+(
+	ReciboID int primary key not null,
+	ListaPreciosID int not null,
+	SocioID int not null,
+	MetodoPago varchar(50) not null,
+	Fecha Datetime not null,
+	
+	constraint chkMetodoPagoRecibo check( MetodoPago in('Efectivo','Tarjeta','Cheque') ),
 
-
+	constraint fkReciboListaPreciosID foreign key (ListaPreciosID) references Pruebas.ListaPrecios(ListaPreciosID),
+	constraint fkReciboSocio foreign key (SocioID) references Pruebas.Socio(SocioID)
 )
 
-Create Table Pruebas.ReciboDetalle(
-ReciboID int  primary key not null,
-ProductoID int not null,
-ImpuestoID int not null,
-BodegaID int not null,
-Cantidad int not null,
-Precio	decimal(10,2) not null default 0 check (Precio >= 0),
-Total decimal(10,2) not null default 0 check (Total >= 0),
 
-constraint fkReciboDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
-constraint fkReciboDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
-constraint fkReciboDetalleBodega2 foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
+Create Table Pruebas.ReciboDetalle --ya creada
+(
+	ReciboID int not null,
+	ProductoID int not null,
+	ImpuestoID int not null,
+	BodegaID int not null,
+	Cantidad int not null,
+	Precio	decimal(10,2) not null default 0 check (Precio >= 0),
+	Total decimal(10,2) not null default 0 check (Total >= 0),
 
+	constraint pkReciboDetalle primary key (ReciboID, ProductoID),
 
-
-
+	constraint fkReciboDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
+	constraint fkReciboDetalleImpuesto foreign key (ImpuestoID) references Pruebas.Impuesto(ImpuestoID),
+	constraint fkReciboDetalleBodega2 foreign key (BodegaID) references Pruebas.Bodega(BodegaID)
 )
 
---pemdiemye las llaves 
-create Table Pruebas.SalidaProducto(
 
-SalidaID int primary key not null,
-SocioID int not null,
-BodegaID int not null,
-constraint fkSalidaProductoSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
-constraint fkSalidaProductoBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
-
-
-
-)
-
-create table Pruebas.SalidaProductoDetalle(
+create Table Pruebas.SalidaProducto  --ya creada
+(
 	SalidaID int primary key not null,
+	SocioID int not null,
+	BodegaID int not null,
+	constraint fkSalidaProductoSocio foreign key (SocioID) references Pruebas.Socio(SocioID),
+	constraint fkSalidaProductoBodega foreign key (BodegaID) references Pruebas.Bodega(BodegaID),
+)
+
+create table Pruebas.SalidaProductoDetalle --ya creada
+(
+	SalidaID int not null,
 	ProductoID int not null,
 	Cantidad int not null,
 
-	constraint fkSalidaProductoDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID),
-
-
+	constraint pkSalidaProductoDetalle primary key (SalidaID, ProductoID),
+	constraint fkSalidaProductoDetalleProducto foreign key (ProductoID) references Pruebas.Producto(ProductoID)
 )
-
-drop table Pruebas.FacturaDetalle
