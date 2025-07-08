85d30133e233d7aabab3b817afbbe78092e6be04
Daniel Mejia <daniel.cuellar@unah.hn>
2025-07-08 15:33:46 -0600

    Agregar Estructura Proyecto

--- obj/Debug/net8.0-windows/AgroLink.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AgroLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28242d04a8474d83a3ae463757b4fe8b215a5a45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1413cd43974953fa162a2ada03db335ef005c75d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
