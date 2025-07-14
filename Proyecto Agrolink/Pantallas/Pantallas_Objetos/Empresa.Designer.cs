b34700286c510b5c4e53e6355c65bf7293cac63e
Daniel Mejia <daniel.cuellar@unah.hn>
2025-07-14 11:29:02 -0600

    Editar distribucion de formulario empresa

--- Proyecto Agrolink/Pantallas/Pantallas_Objetos/Empresa.Designer.cs
+++ Proyecto Agrolink/Pantallas/Pantallas_Objetos/Empresa.Designer.cs
@@ -31,6 +31,12 @@
             components = new System.ComponentModel.Container();
             textBox1 = new TextBox();
             splitContainer1 = new SplitContainer();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
             richTextBox1 = new RichTextBox();
             button3 = new Button();
             button2 = new Button();
@@ -88,7 +94,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(117, 164);
+            textBox1.Location = new Point(52, 124);
             textBox1.Margin = new Padding(2, 3, 2, 3);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
@@ -104,6 +110,12 @@
             // 
             // splitContainer1.Panel1
             // 
+            splitContainer1.Panel1.Controls.Add(label10);
+            splitContainer1.Panel1.Controls.Add(label9);
+            splitContainer1.Panel1.Controls.Add(label8);
+            splitContainer1.Panel1.Controls.Add(textBox5);
+            splitContainer1.Panel1.Controls.Add(comboBox2);
+            splitContainer1.Panel1.Controls.Add(comboBox1);
             splitContainer1.Panel1.Controls.Add(richTextBox1);
             splitContainer1.Panel1.Controls.Add(button3);
             splitContainer1.Panel1.Controls.Add(button2);
@@ -122,24 +134,82 @@
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(splitContainer2);
-            splitContainer1.Size = new Size(2238, 1244);
-            splitContainer1.SplitterDistance = 832;
+            splitContainer1.Size = new Size(2548, 1244);
+            splitContainer1.SplitterDistance = 1180;
             splitContainer1.SplitterWidth = 5;
             splitContainer1.TabIndex = 5;
             // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label10.Location = new Point(52, 694);
+            label10.Margin = new Padding(2, 0, 2, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(223, 41);
+            label10.TabIndex = 23;
+            label10.Text = "Departamento";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label9.Location = new Point(52, 840);
+            label9.Margin = new Padding(2, 0, 2, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(161, 41);
+            label9.TabIndex = 22;
+            label9.Text = "Municipio";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label8.Location = new Point(52, 535);
+            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(125, 41);
+            label8.TabIndex = 21;
+            label8.Text = "Colonia";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(52, 606);
+            textBox5.Margin = new Padding(2, 3, 2, 3);
+            textBox5.Name = "textBox5";
+            textBox5.ReadOnly = true;
+            textBox5.Size = new Size(604, 47);
+            textBox5.TabIndex = 20;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(52, 908);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(604, 49);
+            comboBox2.TabIndex = 19;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(52, 756);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(604, 49);
+            comboBox1.TabIndex = 18;
+            // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(117, 790);
+            richTextBox1.Location = new Point(692, 606);
             richTextBox1.Margin = new Padding(7, 8, 7, 8);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(604, 127);
+            richTextBox1.Size = new Size(438, 199);
             richTextBox1.TabIndex = 17;
             richTextBox1.Text = "";
             // 
             // button3
             // 
-            button3.Location = new Point(586, 1112);
+            button3.Location = new Point(931, 1098);
             button3.Margin = new Padding(2, 3, 2, 3);
             button3.Name = "button3";
             button3.Size = new Size(187, 57);
@@ -151,7 +221,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(347, 1112);
+            button2.Location = new Point(692, 1098);
             button2.Margin = new Padding(2, 3, 2, 3);
             button2.Name = "button2";
             button2.Size = new Size(187, 57);
@@ -163,10 +233,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(169, 965);
+            button1.Location = new Point(115, 1064);
             button1.Margin = new Padding(2, 3, 2, 3);
             button1.Name = "button1";
-            button1.Size = new Size(500, 57);
+            button1.Size = new Size(400, 57);
             button1.TabIndex = 14;
             button1.Text = "Editar Datos de Empresa";
             button1.UseVisualStyleBackColor = true;
@@ -176,18 +246,18 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label5.Location = new Point(81, 741);
+            label5.Location = new Point(713, 535);
             label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(150, 41);
+            label5.Size = new Size(118, 41);
             label5.TabIndex = 13;
-            label5.Text = "Direccion";
+            label5.Text = "Detalle";
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label4.Location = new Point(81, 571);
+            label4.Location = new Point(602, 233);
             label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
             label4.Size = new Size(140, 41);
@@ -196,18 +266,18 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(117, 631);
+            textBox4.Location = new Point(602, 293);
             textBox4.Margin = new Padding(2, 3, 2, 3);
             textBox4.Name = "textBox4";
             textBox4.ReadOnly = true;
-            textBox4.Size = new Size(604, 47);
+            textBox4.Size = new Size(415, 47);
             textBox4.TabIndex = 10;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label3.Location = new Point(81, 413);
+            label3.Location = new Point(52, 389);
             label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
             label3.Size = new Size(113, 41);
@@ -216,7 +286,7 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(117, 470);
+            textBox3.Location = new Point(52, 446);
             textBox3.Margin = new Padding(2, 3, 2, 3);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
@@ -227,7 +297,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label2.Location = new Point(81, 273);
+            label2.Location = new Point(52, 235);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
             label2.Size = new Size(79, 41);
@@ -236,18 +306,18 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(117, 331);
+            textBox2.Location = new Point(52, 293);
             textBox2.Margin = new Padding(2, 3, 2, 3);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(604, 47);
+            textBox2.Size = new Size(415, 47);
             textBox2.TabIndex = 6;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label1.Location = new Point(81, 107);
+            label1.Location = new Point(42, 64);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
             label1.Size = new Size(304, 41);
@@ -269,7 +339,7 @@
             // splitContainer2.Panel2
             // 
             splitContainer2.Panel2.Controls.Add(splitContainer3);
-            splitContainer2.Size = new Size(1401, 1244);
+            splitContainer2.Size = new Size(1363, 1244);
             splitContainer2.SplitterDistance = 620;
             splitContainer2.SplitterWidth = 3;
             splitContainer2.TabIndex = 1;
@@ -288,7 +358,7 @@
             // splitContainer4.Panel2
             // 
             splitContainer4.Panel2.Controls.Add(dataGridView1);
-            splitContainer4.Size = new Size(1401, 620);
+            splitContainer4.Size = new Size(1363, 620);
             splitContainer4.SplitterDistance = 85;
             splitContainer4.TabIndex = 1;
             // 
@@ -397,7 +467,7 @@
             // splitContainer3.Panel2
             // 
             splitContainer3.Panel2.Controls.Add(dataGridView2);
-            splitContainer3.Size = new Size(1401, 621);
+            splitContainer3.Size = new Size(1363, 621);
             splitContainer3.SplitterDistance = 67;
             splitContainer3.TabIndex = 3;
             // 
@@ -453,26 +523,26 @@
             contextMenuStrip2.ImageScalingSize = new Size(40, 40);
             contextMenuStrip2.Items.AddRange(new ToolStripItem[] { borrarToolStripMenuItem1, editarToolStripMenuItem1 });
             contextMenuStrip2.Name = "contextMenuStrip2";
-            contextMenuStrip2.Size = new Size(361, 155);
+            contextMenuStrip2.Size = new Size(177, 100);
             // 
             // borrarToolStripMenuItem1
             // 
             borrarToolStripMenuItem1.Name = "borrarToolStripMenuItem1";
-            borrarToolStripMenuItem1.Size = new Size(360, 48);
+            borrarToolStripMenuItem1.Size = new Size(176, 48);
             borrarToolStripMenuItem1.Text = "Borrar";
             borrarToolStripMenuItem1.Click += borrarToolStripMenuItem1_Click;
             // 
             // editarToolStripMenuItem1
             // 
             editarToolStripMenuItem1.Name = "editarToolStripMenuItem1";
-            editarToolStripMenuItem1.Size = new Size(360, 48);
+            editarToolStripMenuItem1.Size = new Size(176, 48);
             editarToolStripMenuItem1.Text = "Editar";
             // 
             // Empresa
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2238, 1244);
+            ClientSize = new Size(2548, 1244);
             Controls.Add(splitContainer1);
             Margin = new Padding(2, 3, 2, 3);
             Name = "Empresa";
@@ -541,5 +611,11 @@
         private DataGridViewTextBoxColumn ImpuestoID;
         private DataGridViewTextBoxColumn Nombre;
         private DataGridViewTextBoxColumn Valor;
+        private Label label8;
+        private TextBox textBox5;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label10;
+        private Label label9;
     }
 }
